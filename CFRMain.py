b029801d1a613128ad31ec2722535f9fcfc07dcf
Said Mamirov <sm67@stmarys-ca.edu>
2022-10-29 15:53:17 -0700

    Second Commit

--- CFRMain.py
+++ CFRMain.py
@@ -3,12 +3,18 @@ from nltk.tokenize import TreebankWordTokenizer
 from collections import Counter
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS as sklearnStopWords
 
+# This functions strips all tags from the XML Document
+# and creates a text file without the tags.
+# Ex. <Title>, <Head>, etc.
 def stripXMLTags(xmlFile, outputFile):
     text = re.sub('<[^<]+>', "", open(xmlFile).read())
     with open("output.txt", "w") as f:
         f.write(text)
     print(len(text))
 
+# This functions creates a token sequence from a text file and writes to 
+# a separate file.
+# Also doesn't add punctuations (can add them by commenting out lines 27 and 28).
 def tokenizing(textFile, tokenSequenceOutput):
     with open(textFile, "r") as file1:
         doc = file1.read()
@@ -16,13 +22,14 @@ def tokenizing(textFile, tokenSequenceOutput):
     tokenizer = TreebankWordTokenizer()
     tokenSequence = tokenizer.tokenize(doc.lower())
     punc = '''!()-[]{};:'"\, <>./?@#$%^&*_~'''
-    #with open(tokenSequenceOutput, "w") as file2:
-        #file2.write(str(tokenSequence))
+
     with open(tokenSequenceOutput, 'w') as f:
         for line in tokenSequence:
             if line not in punc:
                 f.write(f"{line}\n")
 
+# This functions gets the word frequency including stop words.
+# Writes the results to a text file.
 def termFrequencyWithStopWords(tokenSequenceFile, termFrequencyOutput):
     with open(tokenSequenceFile, "r") as file3:
         tokenSeq = file3.read()
@@ -32,6 +39,7 @@ def termFrequencyWithStopWords(tokenSequenceFile, termFrequencyOutput):
     with open(termFrequencyOutput, "w") as file4:
         file4.write(str(bagOfWords))
 
+# Same as above function except it leaves out stop words.
 def termFrequencyWithoutStopWords(tokenSequenceFile, termFrequencyOutput):
     with open(tokenSequenceFile, "r") as file3:
         tokenSeq = file3.read()
@@ -43,10 +51,12 @@ def termFrequencyWithoutStopWords(tokenSequenceFile, termFrequencyOutput):
     with open(termFrequencyOutput, "w") as file5:
         file5.write(str(bagOfWords))
 
+# A function that writes all the stopwords to a text file.
 def stopWords():
     with open("stopwords.txt", "w") as file6:
         file6.write(str(sklearnStopWords))
 
+# Uncomment the below lines to run 
 '''stripXMLTags("ECFR-title47.xml", "ECFR-output.txt")
 stopWords()
 tokenizing("ECFR-output.txt", "ECFR-tokenSeq.txt")
